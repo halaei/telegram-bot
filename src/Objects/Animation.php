fe70ba5df7e25e1efb79191dbd29a172bbdc2bdb
Hamid Alaei V <hamid.a85@gmail.com>
2016-10-03 19:59:23 +0330

    gaming platform

---
+++ src/Objects/Animation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Telegram\Bot\Objects;
+
+/**
+ * Class Animation.
+ *
+ *
+ * @method string       getFileId()     Unique file identifier.
+ * @method PhotoSize    getThumb()      (Optional). Document thumbnail as defined by sender.
+ * @method string       getFileName()   (Optional). Original filename as defined by sender.
+ * @method string       getMimeType()   (Optional). MIME type of the file as defined by sender.
+ * @method int          getFileSize()   (Optional). File size.
+ */
+class Animation extends Document
+{
+}
---
+++ src/Objects/CallbackGame.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Telegram\Bot\Objects;
+
+/**
+ * Class CallbackGame.
+ * A placeholder, currently holds no information. Use BotFather to set up your game.
+ */
+class CallbackGame extends BaseObject
+{
+    /**
+     * Property relations.
+     *
+     * @return array
+     */
+    public function relations()
+    {
+        return [];
+    }
+}
