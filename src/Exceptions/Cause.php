c2c586acb3cd362c38718e3251dba4a97feaf752
halaei <hamid.a85@gmail.com>
2016-12-03 17:59:17 +0330

    let cause methods return closure

--- src/Exceptions/Cause.php
+++ src/Exceptions/Cause.php
@@ -6,38 +6,62 @@ use Illuminate\Support\Str;
 
 class Cause
 {
-    public static function userBlocked(\Exception $e)
+    public static function userBlocked(\Exception $e = null)
     {
-        return $e instanceof TelegramResponseException &&
-        Str::contains($e->getMessage(), ['blocked', 'User is deactivated']);
+        $closure = function (\Exception $e) {
+            return $e instanceof TelegramResponseException &&
+            Str::contains($e->getMessage(), ['blocked', 'User is deactivated']);
+        };
+
+        return is_null($e) ? $closure : $closure($e);
     }
 
-    public static function botWasKicked(\Exception $e)
+    public static function botWasKicked(\Exception $e = null)
     {
-        return $e instanceof TelegramResponseException &&
-        Str::contains($e->getMessage(), 'bot was kicked');
+        $closure = function (\Exception $e) {
+            return $e instanceof TelegramResponseException &&
+            Str::contains($e->getMessage(), 'bot was kicked');
+        };
+
+        return is_null($e) ? $closure : $closure($e);
     }
 
-    public static function botWasBlockedOrKicked(\Exception $e)
+    public static function botWasBlockedOrKicked(\Exception $e = null)
     {
-        return self::userBlocked($e) or self::botWasKicked($e);
+        $closure = function (\Exception $e) {
+            return self::userBlocked($e) or self::botWasKicked($e);
+        };
+
+        return is_null($e) ? $closure : $closure($e);
     }
 
-    public static function invalidChat(\Exception $e)
+    public static function invalidChat(\Exception $e = null)
     {
-        return $e instanceof TelegramResponseException &&
-        Str::contains($e->getMessage(), '[Error]: Bad Request: chat not found');
+        $closure = function (\Exception $e) {
+            return $e instanceof TelegramResponseException &&
+            Str::contains($e->getMessage(), '[Error]: Bad Request: chat not found');
+        };
+
+        return is_null($e) ? $closure : $closure($e);
     }
 
-    public static function invalidFileId(\Exception $e)
+    public static function invalidFileId(\Exception $e = null)
     {
-        return $e instanceof TelegramResponseException &&
-        Str::contains($e->getMessage(), 'Wrong file identifier');
+        $closure = function (\Exception $e) {
+            return $e instanceof TelegramResponseException &&
+            Str::contains($e->getMessage(), 'Wrong file identifier');
+        };
+
+        return is_null($e) ? $closure : $closure($e);
     }
 
-    public static function messageNotModified(\Exception $e)
+    public static function messageNotModified(\Exception $e = null)
     {
-        return $e instanceof TelegramResponseException &&
-        Str::contains($e->getMessage(), 'message is not modified');
+        $closure = function (\Exception $e) {
+            return $e instanceof TelegramResponseException &&
+            Str::contains($e->getMessage(), 'message is not modified');
+        };
+
+        return is_null($e) ? $closure : $closure($e);
     }
 }
