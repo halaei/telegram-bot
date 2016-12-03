8af507d810f7dbe2ce0a5871a1e74359796f75e5
halaei <hamid.a85@gmail.com>
2016-12-03 17:39:46 +0330

    Cause: check instance of TelegramResponseException

--- src/Exceptions/Cause.php
+++ src/Exceptions/Cause.php
@@ -8,13 +8,13 @@ class Cause
 {
     public static function userBlocked(\Exception $e)
     {
-        return $e instanceof TelegramSDKException &&
+        return $e instanceof TelegramResponseException &&
         Str::contains($e->getMessage(), ['blocked', 'User is deactivated']);
     }
 
     public static function botWasKicked(\Exception $e)
     {
-        return $e instanceof TelegramSDKException &&
+        return $e instanceof TelegramResponseException &&
         Str::contains($e->getMessage(), 'bot was kicked');
     }
 
@@ -25,19 +25,19 @@ class Cause
 
     public static function invalidChat(\Exception $e)
     {
-        return $e instanceof TelegramSDKException &&
+        return $e instanceof TelegramResponseException &&
         Str::contains($e->getMessage(), '[Error]: Bad Request: chat not found');
     }
 
     public static function invalidFileId(\Exception $e)
     {
-        return $e instanceof TelegramSDKException &&
+        return $e instanceof TelegramResponseException &&
         Str::contains($e->getMessage(), 'Wrong file identifier');
     }
 
     public static function messageNotModified(\Exception $e)
     {
-        return $e instanceof TelegramSDKException &&
+        return $e instanceof TelegramResponseException &&
         Str::contains($e->getMessage(), 'message is not modified');
     }
 }
